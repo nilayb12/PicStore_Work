a37affe96db74dbb52eaacaab82b2a45461105b3
nilayb12 <65295315+nilayb12@users.noreply.github.com>
2024-05-28 12:58:42 +0530

    Minor Edit

--- modules/imgContainer.php
+++ modules/imgContainer.php
@@ -9,10 +9,14 @@
     $result = mysqli_query($db, $query);
 
     if (mysqli_num_rows($result) == 0) {
-        // file_get_contents('emptyfolder.svg', true)
         echo '<div class="pt-5 text-center">';
         if (empty($path)) { ?>
-            <img class="my-3" src="./Reliance_Jio_Logo.svg" width="160rem" height="160rem"></img>
+            <svg class="my-2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="10rem" height="10rem">
+                <circle cx="12" cy="12" r="12" fill="#0a2885"></circle>
+                <path
+                    d="M17.587 14.559c-.883 0-1.49-.648-1.49-1.574 0-.912.62-1.56 1.49-1.56s1.491.648 1.491 1.573c0 .897-.634 1.56-1.49 1.56zm.03-5.152c-2.265 0-3.772 1.437-3.772 3.576 0 2.195 1.451 3.604 3.729 3.604 2.264 0 3.755-1.409 3.755-3.59 0-2.153-1.475-3.59-3.713-3.59zM11.78 6.272c-.856 0-1.395.483-1.395 1.243 0 .774.552 1.257 1.435 1.257.857 0 1.395-.483 1.395-1.257 0-.773-.552-1.243-1.435-1.243zm.152 3.204h-.277c-.675 0-1.187.317-1.187 1.285v4.42c0 .98.496 1.284 1.216 1.284h.275c.677 0 1.16-.33 1.16-1.285v-4.419c0-.995-.47-1.285-1.187-1.285zM8.316 7.392h-.4c-.76 0-1.174.43-1.174 1.285v4.13c0 1.063-.36 1.436-1.2 1.436-.662 0-1.201-.29-1.63-.816C3.87 13.373 3 13.786 3 14.81c0 1.104 1.035 1.781 2.955 1.781 2.334 0 3.563-1.173 3.563-3.742V8.675c0-.856-.413-1.283-1.202-1.283z"
+                    fill="#fff"></path>
+            </svg>
             <h1>Home Page</h1>
             <p class="mx-auto fs-5 text-muted">
                 Navigate to SAP/Site IDs using Above Controls to View Image(s).
@@ -21,14 +25,13 @@
             <h1 class="my-5"><i class="bi bi-file-earmark-x-fill"></i></h1>
             <p class="mx-auto fs-5 text-muted">
                 No Image(s) for <code><?php echo substr($path, 8); ?></code>.<br>
-                Try Uploading some or Navigate to SAP/Site IDs using Above Controls.<br>
+                Try Uploading some or Navigate to other SAP/Site IDs using Above Controls.<br>
                 Reload to GoTo Home Page.
             </p>
         <?php }
         echo '</div>';
     } else {
-        while ($data = mysqli_fetch_assoc($result)) {
-            ?>
+        while ($data = mysqli_fetch_assoc($result)) { ?>
             <figure class="!figure card border-secondary" id="figBox">
                 <img class="!figure-img card-img-top img-fluid" title="Click to Zoom" id="figImg"
                     src="<?php echo 'images/' . $data['FilePath'] . '/' . $data['FileName']; ?>">
@@ -64,8 +67,7 @@
                 ?>
             </div> -->
             </figure>
-            <?php
-        }
+        <?php }
     }
     ?>
 </form>
